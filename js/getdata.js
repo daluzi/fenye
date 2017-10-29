b6e973cfff1fd6cee34592cc9af542fd9dd93484
daluzi <1210415250@qq.com>
2017-10-29 11:35:03 +0800

    first commit

---
+++ js/getdata.js
@@ -0,0 +1,27 @@
+// var limit = 0,
+// 	row = 10;
+// $(function($) {
+// 	var url = 'http://123.207.11.231:8080/SCIEManagement/article/manage';
+// 	$.ajax({
+// 		url: url,
+// 		type: "POST",
+// 		data: {
+// 			'limit': limit,
+// 			'row': row
+// 		},
+// 		success: function(data) {
+// 				// console.log(data.articles.article);
+// 				render(data.articles.article);
+// 			}
+// 	})
+// })
+// function render(datalist) {
+// 	var str = '';
+// 	var addCont = $("#content");
+// 	$.each(datalist,function(i) {
+// 		// console.log(datalist[i].title);
+// 		str += "<p class = "+"title"+">" + datalist[i].title + "</p>";
+// 		$("#content").html(str);
+// 	})
+// }
+
